7b5d41ad703f530968a902dc0a32aa789533fe11
Chinmay Dalal <chinmay.dalal.22012001@gmail.com>
2020-10-11 14:16:00 +0530

    Rename

--- build.fish
+++ build.fish
@@ -1,22 +1,22 @@
 #! /usr/bin/fish
 echo Removing old outputs...
 
-rm -rf Adwaita-One-Dark/gnome-shell/
-rm -rf Adwaita-One-Dark/gtk-3.0/
+rm -rf Adwaita-Dracula/gnome-shell/
+rm -rf Adwaita-Dracula/gtk-3.0/
 
 echo Generating the GNOME Shell theme...
 
-mkdir -p Adwaita-One-Dark/gnome-shell
-sassc src/gnome-shell/gnome-shell.scss Adwaita-One-Dark/gnome-shell/gnome-shell.css
-cp src/gnome-shell/pad-osd.css Adwaita-One-Dark/gnome-shell/pad-osd.css
+mkdir -p Adwaita-Dracula/gnome-shell
+sassc src/gnome-shell/gnome-shell.scss Adwaita-Dracula/gnome-shell/gnome-shell.css
+cp src/gnome-shell/pad-osd.css Adwaita-Dracula/gnome-shell/pad-osd.css
 
 echo Generating the GTK theme...
 
-mkdir -p Adwaita-One-Dark/gtk-3.0
+mkdir -p Adwaita-Dracula/gtk-3.0
 cp src/gtk-3.0/_drawing.scss src/libhandy-themes/
 cp src/gtk-3.0/_colors.scss src/libhandy-themes/
 
-sassc -M -t compact src/gtk-3.0/gtk-contained-dark.scss Adwaita-One-Dark/gtk-3.0/gtk.css
-cp -r src/gtk-3.0/assets Adwaita-One-Dark/gtk-3.0
+sassc -M -t compact src/gtk-3.0/gtk-contained-dark.scss Adwaita-Dracula/gtk-3.0/gtk.css
+cp -r src/gtk-3.0/assets Adwaita-Dracula/gtk-3.0
 
 echo done.
